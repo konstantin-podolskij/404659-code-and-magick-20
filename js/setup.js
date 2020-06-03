1871587efc91569e704441616f362fc1061fdabf
Konstantin Podolsckij <kpodolsckij@yandex.ru>
2020-06-04 01:40:44 +0300

    Добавлены функции случайного имени и цвета пальто

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,32 @@
 'use strict';
 
-var WIZARD_NAMES = ['Дамблдор', 'Волдеморт', 'Доктор Стрендж', 'Гарри Поттер'];
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+
+var getRandomNumber = function (max) {
+  var rand = Math.random() * max;
+  return Math.floor(rand);
+};
+
+var getRandomName = function () {
+  return WIZARD_NAMES[getRandomNumber(WIZARD_NAMES.length)];
+};
+
+var getRandomSurname = function () {
+  return WIZARD_SURNAMES[getRandomNumber(WIZARD_SURNAMES.length)];
+};
+
+var getRandomCoatColor = function () {
+  return COAT_COLORS[getRandomNumber(COAT_COLORS.length)];
+};
+
+var getWizardFullname = function () {
+  var wizardFullname = getRandomName() + ' ' + getRandomSurname();
+
+  return wizardFullname;
+};
+
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
@@ -9,23 +35,22 @@ document.querySelector('.setup-similar').classList.remove('hidden');
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-
 var wizards = [
   {
-    name: WIZARD_NAMES[0],
-    coatColor: 'rgb(241, 43, 107)'
+    name: getWizardFullname(),
+    coatColor: getRandomCoatColor()
   },
   {
-    name: WIZARD_NAMES[1],
-    coatColor: 'rgb(215, 210, 55)'
+    name: getWizardFullname(),
+    coatColor: getRandomCoatColor()
   },
   {
-    name: WIZARD_NAMES[2],
-    coatColor: 'rgb(101, 137, 164)'
+    name: getWizardFullname(),
+    coatColor: getRandomCoatColor()
   },
   {
-    name: WIZARD_NAMES[3],
-    coatColor: 'rgb(127, 127, 127)'
+    name: getWizardFullname(),
+    coatColor: getRandomCoatColor()
   }
 ];
 
