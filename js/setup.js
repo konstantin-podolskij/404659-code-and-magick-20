072dca960fa3aa8e23d8954ec0198749e8e004ba
Konstantin Podolsckij <kpodolsckij@yandex.ru>
2020-06-04 15:43:45 +0300

    Рефакторинг

--- js/setup.js
+++ js/setup.js
@@ -4,33 +4,25 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var MAX_WIZARD_COUNT = 4;
+
 var wizards = [];
+var userDialog = document.querySelector('.setup');
+var similarBlock = userDialog.querySelector('.setup-similar');
+var similarListElement = similarBlock.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
 var getRandomNumber = function (max) {
   var rand = Math.random() * max;
   return Math.floor(rand);
 };
 
-var getRandomName = function () {
-  return WIZARD_NAMES[getRandomNumber(WIZARD_NAMES.length)];
+var getRandomValue = function (array) {
+  return array[getRandomNumber(array.length)];
 };
 
-var getRandomSurname = function () {
-  return WIZARD_SURNAMES[getRandomNumber(WIZARD_SURNAMES.length)];
-};
-
-var getRandomCoatColor = function () {
-  return COAT_COLORS[getRandomNumber(COAT_COLORS.length)];
-};
-
-var getRandomEyesColor = function () {
-  return EYES_COLORS[getRandomNumber(EYES_COLORS.length)];
-};
-
-var getRandomWizardFullname = function () {
-  var wizardFullname = getRandomName() + ' ' + getRandomSurname();
-
-  return wizardFullname;
+var getRandomWizardFullname = function (arrNames, arrSurnames) {
+  return getRandomValue(arrNames) + ' ' + getRandomValue(arrSurnames);
 };
 
 var renderWizard = function (currentWizard) {
@@ -42,24 +34,27 @@ var renderWizard = function (currentWizard) {
   similarListElement.appendChild(wizardElement);
 };
 
-var userDialog = document.querySelector('.setup');
+var fillArrayWizard = function () {
+  for (var j = 0; j < MAX_WIZARD_COUNT; j++) {
+    var wizard = {
+      name: getRandomWizardFullname(WIZARD_NAMES, WIZARD_SURNAMES),
+      coatColor: getRandomValue(COAT_COLORS),
+      eyesColor: getRandomValue(EYES_COLORS)
+    };
+    wizards.push(wizard);
+  }
+
+  return wizards;
+};
+
+var renderSimilarList = function (array) {
+  for (var i = 0; i < array.length; i++) {
+    renderWizard(wizards[i]);
+  }
+};
+
 userDialog.classList.remove('hidden');
+similarBlock.classList.remove('hidden');
 
-document.querySelector('.setup-similar').classList.remove('hidden');
+renderSimilarList(fillArrayWizard());
 
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-
-
-for (var j = 0; j < 4; j++) {
-  var wizard = {
-    name: getRandomWizardFullname(),
-    coatColor: getRandomCoatColor(),
-    eyesColor: getRandomEyesColor()
-  };
-  wizards.push(wizard);
-}
-
-for (var i = 0; i < wizards.length; i++) {
-  renderWizard(wizards[i]);
-}
